889c7f86a7e28b86142a5289ee114d9f3dc55f8c
UxioMendez <uxio.mendez@hotmail.com>
2024-06-03 20:25:56 +0200

    [11] Añadidos métodos

--- Roadmap/11 - MANEJO DE FICHEROS/python/UxioMendezPRO.py
+++ Roadmap/11 - MANEJO DE FICHEROS/python/UxioMendezPRO.py
@@ -1,7 +1,7 @@
 # Manejo de ficheros
 
 
-class Producto:
+class Product:
     def __init__(self, nombre_producto, cantidad_vendida, precio) -> None:
         pass
         self.__nombre_producto = nombre_producto
@@ -18,16 +18,40 @@ class Producto:
         return self.__precio
 
 
+product_list = []
+
+
 def anadir():
     nombre_producto = input("Nombre")
     cantidad_vendida = input("Cantidad vendida")
     precio = input("Precio")
-    producto = Producto(nombre_producto, cantidad_vendida, precio)
-    return producto
+    product = Product(nombre_producto, cantidad_vendida, precio)
+    product_list.append(product)
+    return product
 
 
-i = 0
-while i == 0:
+def consultar():
+    for product in product_list:
+        print(product)
+
+
+def actualizar():
+    return product_list
+
+
+def eliminar():
+    for product in product_list:
+        product_list.remove(product)
+    return product_list
+
+
+def salir():
+    salir = True
+    return salir
+
+
+salir = False
+while salir == False:
     menu = input(
         "1. Añadir \n 2. Consultar \n 3.Actualizar \n 4.Eliminar Productos \n 5. Salir"
     )
